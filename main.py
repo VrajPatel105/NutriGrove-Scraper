69baec3447f6da8d6f25c3f83b14d89017adfa37
Vraj Patel <vrajp1052005@gmail.com>
2025-08-31 18:58:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ try:
         print("Weekday detected - scraping breakfast, lunch, and dinner")
         scraper.fetch_breakfast()
         scraper.fetch_lunch()
-        scraper.fetch_dinner()
+        scraper.fetch_dinner() 
 finally:
     scraper.close()
 
 # once the scraping is done, now we need to clean the scraped data.
 cleaner = FoodDataCleaner()
 cleaner.clean_food_data()
+
 
 # now tht we have stored the cleaned data to our dir, now we can finally go ahead and upload all the cleaned data to the db.

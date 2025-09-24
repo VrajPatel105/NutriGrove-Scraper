77f25b075ec4dbdb41ab86b74c2cd25596dd9ab1
Vraj Patel <vrajp1052005@gmail.com>
2025-09-24 14:05:46 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -34,7 +34,7 @@ class Scraper:
 
         # Configure Chrome options for stealth operation
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
